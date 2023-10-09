beaa0bc2ae93b8a880b8934ca7e4b02a31d279be
hadihage <145401012+hadihage@users.noreply.github.com>
2023-10-09 18:18:02 -0400

    created Driver class

--- projdir/src/projectgit/Driver.java
+++ projdir/src/projectgit/Driver.java
@@ -3,7 +3,7 @@ package projectgit;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Our team is just getting started");
 
 	}
 
